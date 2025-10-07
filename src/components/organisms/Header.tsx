01567f5da405588e227da99e929ef1fa5188af8f
miho.sarugaku <miho.sarugaku@novel.jpn.com>
2025-10-07 14:59:07 +0900

    fix: ヘッダー修正

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,23 +1,25 @@
-import { Box, Heading, HStack, Spacer } from '@chakra-ui/react'
+import { Box, Heading, HStack, Spacer, Container } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <>
       <Box p={4} zIndex='100px' borderBottom='1px solid' borderColor='gray.200'>
-        <HStack>
-          <Link to={'/'}>
-            <Heading size='2xl' pl='100px' color='gray.500'>
-              NOVEL HELPDESK
-            </Heading>
-          </Link>
-          <Spacer />
-          <Link to={'/'}>
-            <Heading size='2xl' pr='300px' color='gray.500'>
-              Ticket
-            </Heading>
-          </Link>
-        </HStack>
+        <Container>
+          <HStack>
+            <Link to={'/'}>
+              <Heading size='2xl' color='gray.500'>
+                NOVEL HELPDESK
+              </Heading>
+            </Link>
+            <Spacer />
+            <Link to={'/'}>
+              <Heading size='2xl' pr='300px' color='gray.500'>
+                Ticket
+              </Heading>
+            </Link>
+          </HStack>
+        </Container>
       </Box>
     </>
   )
